c17f3fb143f32b38ade4fc560c19753e7ec6a79f
0xC0ba1t <adiadur@gmail.com>
2024-11-28 19:08:51 +0000

    set up DistributedDataParallel

--- redo/train_model.py
+++ redo/train_model.py
@@ -4,6 +4,7 @@ import torch
 import torch.nn as nn
 from torch.nn import functional as F
 import inspect
+import os
 
 # -----------------------------------------------------------------------------
 
@@ -242,15 +243,37 @@ class DataLoaderLite:
         return x, y
 
 # -----------------------------------------------------------------------------
-# attempt to autodetect the type of device available for PyTorch, cpu/CUDA
 import time
 
-device = "cpu"
-if torch.cuda.is_available():
-    device = "cuda"
-elif hasattr(torch.backends, "mps") and torch.backends.mps.is_available():
-    device = "mps"
-print(f"using device: {device}")
+# run the training loop (if multi GPU avail)
+from torch.distributed import init_process_group, destroy_process_group
+
+# set up DDP (distributed data parallel)
+# torchrun command sets the env vars RANK, LOCAL_RANK, and WORLD_SIZE
+ddp = int(os.environ.get('RANK', -1)) != -1 # is this run a DDP run?
+if ddp:
+    # use of DDP requires (based on docs from some while back) CUDA, so we set appropriate device according to rank
+    assert torch.cuda.is_available(), "for now i think we need CUDA for DDP"
+    init_process_group(backend='nccl')
+    ddp_rank = int(os.environ['RANK'])
+    ddp_local_rank = int(os.environ['LOCAL_RANK'])
+    ddp_world_size = int(os.environ['WORLD_SIZE'])
+    device = f'cuda:{ddp_local_rank}'
+    torch.cuda.set_device(device)
+    master_process = ddp_rank == 0 # this process will do logging, checkpointing etc.
+else:
+    # vanilla (non-DDP run)
+    ddp_rank = 0
+    ddp_local_rank = 0
+    ddp_world_size = 1
+    master_process = True
+    # attempt to auto detect device
+    device = "cpu"
+    if torch.cuda.is_available():
+        device = "cuda"
+    elif hasattr(torch.backends, "mps") and torch.backends.mps.is_available():
+        device = "mps"
+    print(f"using device: {device}")
 
 torch.manual_seed(1337)
 if torch.cuda.is_available():
