859bbafb1a3e81c80df4878b2778e2ba91fca720
0xC0ba1t <adiadur@gmail.com>
2024-12-01 16:08:21 +0000

    maybe final commit in v2, added random shuffling of dataset when loading to prevent periodisation in training, added saving and loading of model, and allow resume of training from a saved checkpoint and more aggressive lr

--- redo/train_model.py
+++ redo/train_model.py
@@ -52,6 +52,10 @@ class CausalSelfAttention(nn.Module):
         y = self.c_proj(y)
         return y
 
+class TanhGELU(nn.Module):
+    def forward(self, input):
+        return 0.5 * input  * (1.0 + torch.tanh(math.sqrt(2.0 / math.pi) * (input + 0.044715 * torch.pow(input, 3.0))))
+
 class MLP(nn.Module):
 
     def __init__(self, config):
@@ -225,26 +229,54 @@ class DataLoaderLite:
         self.T = T
         self.process_rank = process_rank
         self.num_processes = num_processes
+        self.split = split
         assert split in {'train', 'val'}
+        self.rng = np.random.default_rng(1337)
 
-        # get the filename of the shards
+        # get shard filenames
         data_root = "edu_fineweb10B"
         shards = os.listdir(data_root)
         shards = [s for s in shards if split in s]
         shards = sorted(shards)
         shards = [os.path.join(data_root, s) for s in shards]
         self.shards = shards
-        assert len(shards) > 0, f"no shards found for split: {split}"
+        assert len(shards) > 0, f"no shards found for split {split}"
         if master_process:
-            print(f"found {len(shards)} shards for split: {split}")
+            print(f"found {len(shards)} shards for split {split}")
         self.reset()
+
+    def load_shard(self, filename): # added from PR to avoid periodisation in training: https://github.com/karpathy/build-nanogpt/pull/52/files
+        shard = load_tokens(filename)
+        if self.split == 'train':
+            # split tokens into documents using the <|endoftext|> special token and shuffle
+            eot_positions = (torch.where(shard == enc.eot_token)[0] + 1).tolist()
+            documents = [shard[start:end] for start, end in zip([0] + eot_positions[:-1], eot_positions)]
+            self.rng.shuffle(documents)
+            shard = torch.cat(documents) # concatenate the documents back together
+        return shard
+
+    def set(self, loader_checkpoint):
+        self.current_position = loader_checkpoint['current_position'] + self.B * self.T * self.process_rank # we add the B*T*process_rank to the position to make sure it is the correct position for each process
+        self.current_shard = loader_checkpoint['current_shard']
+        self.tokens = load_tokens(self.shards[self.current_shard])
+        # if loading the next batch would be out of bounds, advance to next shard
+        if self.current_position + (B * T * self.num_processes + 1) > len(self.tokens):
+            self.current_shard += 1
+            # reshuffle after each epoch
+            if self.current_shard == len(self.shards):
+                self.reset()
+            else:
+                self.tokens = self.load_shard(self.shards[self.current_shard])
+                self.current_position = B * T * self.process_rank
 
     def reset(self):
         # state, init at shard zero
         self.current_shard = 0
-        self.tokens = load_tokens(self.shards[self.current_shard])
+        if self.split == 'train':
+            self.rng.shuffle(self.shards)
+        self.tokens = self.load_shard(self.shards[self.current_shard])
         self.current_position = self.B * self.T * self.process_rank
-    
+        
     def next_batch(self):
         B, T = self.B, self.T
         buf = self.tokens[self.current_position:self.current_position+B*T+1]
@@ -254,9 +286,12 @@ class DataLoaderLite:
         self.current_position += B * T * self.num_processes
         # if loading the next batch is not possible (out of bounds) then advance to next shard
         if self.current_position + (B * T * self.num_processes + 1) > len(self.tokens):
-            self.current_shard = (self.current_shard + 1) % len(self.shards)
-            self.tokens = load_tokens(self.shards[self.current_shard])
-            self.current_position = B * T * self.process_rank
+            self.current_shard += 1
+            if self.current_shard == len(self.shards):
+                self.reset()
+            else:
+                self.tokens = self.load_shard(self.shards[self.current_shard])
+                self.current_position = B * T * self.process_rank
         return x, y
 
 # -----------------------------------------------------------------------------
@@ -322,6 +357,8 @@ else:
         device = "mps"
     print(f"using device: {device}")
 
+device_type = "cuda" if device.startswith("cuda") else "cpu"
+
 torch.manual_seed(1337)
 if torch.cuda.is_available():
     torch.cuda.manual_seed(1337)
@@ -342,21 +379,68 @@ val_loader = DataLoaderLite(B=B, T=T, process_rank=ddp_rank, num_processes=ddp_w
 
 torch.set_float32_matmul_precision('high') # tf32
 
-# create model
-model = GPT(GPTConfig(vocab_size=50304))
-model.to(device)
-use_compile = False # torch.compile interferes with HellaSwag Eval and generation
+# create a log dir that checkpoints and logs will be written to
+log_dir = "log"
+os.makedirs(log_dir, exist_ok=True)
+log_file = os.path.join(log_dir, f"log.txt")
+with open(log_file, "w") as f: # open file in write mode
+    pass
+
+# Start / Resume Training
+resume_training = True
+if resume_training:
+    # get latest checkpoint file
+    checkpoint_files = [f for f in os.listdir(log_dir) if f.startswith("model_") and f.endswith(".pt")]
+    assert len(checkpoint_files) > 0, "no checkpoints found"
+    checkpoint_files = sorted(checkpoint_files)
+    last_checkpoint = checkpoint_files[-1]
+    checkpoint_path = os.path.join(log_dir, last_checkpoint)
+    checkpoint = torch.load(checkpoint_path, map_location=device)
+    # load model state
+    model = GPT(checkpoint['config'])
+    model.to(device)
+    model.load_state_dict(checkpoint['model'])
+    # load optimizer state
+    optimizer = model.configure_optimizers(weight_decay=0.1, learning_rate=6e-4, device=device_type)
+    optimizer.load_state_dict(checkpoint['optimizer'])
+    # load step (which will also load learning rate)
+    current_step = checkpoint['step'] + 1
+    # load traning data state
+    train_loader.set(checkpoint['train_loader'])
+    if master_process:
+        print(f"resuming training from step {current_step} with a validation loss of {checkpoint['val_loss']:.4f}")
+else:
+    # create model
+    model = GPT(GPTConfig(vocab_size=50304)) # increase vocabsize to make it have more factors of 2
+    model.to(device)
+    current_step = 0
+    optimizer = model.configure_optimizers(weight_decay=0.1, learning_rate=6e-4, device=device_type)
+    # clear the log file
+    with open(log_file, "w") as f: # open for writing to clear the file
+        pass
+
+def unwrap_model(model):
+    # Unwrap DDP
+    if hasattr(model, 'module'):
+        model = model.module
+    # Unwrap torch.compile
+    if hasattr(model, '_orig_mod'):
+        model = model._orig_mod
+    return model
+
+use_compile = True
 if use_compile:
-    print("Using torch.compile")
-    model = torch.compile(model)
+    model = torch.compile(model) # Cannot (per now) do this while generating and evaluating with HellaSwag 
 if ddp:
     model = DDP(model, device_ids=[ddp_local_rank])
-raw_model = model.module if ddp else model # always contains the "raw" unwrapped model
+
+# Use the unwrap_model function to get the raw model
+raw_model = unwrap_model(model)
 
 # lr scheduler
-max_lr = 6e-4
+max_lr = 6e-4 * 2
 min_lr = max_lr * 0.1
-warmup_steps = 715 # (375e6 / 2**19); based off of GPT-3 paper, where warmup is 375M toks, so warmup tok num / toks per step
+warmup_steps = 200
 max_steps = 19073 # (10e9 / 2**19); total toks / toks per step
 def get_lr(it):
     # 1: linear warmup for warmup_iters steps
@@ -370,16 +454,6 @@ def get_lr(it):
     assert 0 <= decay_ratio <= 1
     coeff = 0.5 * (1.0 + math.cos(math.pi * decay_ratio)) # coeff starts at 1 and goes to 0
     return min_lr + coeff * (max_lr - min_lr)
-
-# optimize!
-optimizer = raw_model.configure_optimizers(weight_decay=0.1, learning_rate=6e-4, device=device)
-
-# create a log dir that checkpoints and logs will be written to
-log_dir = "log"
-os.makedirs(log_dir, exist_ok=True)
-log_file = os.path.join(log_dir, f"log.txt")
-with open(log_file, "w") as f: # open file in write mode
-    pass
 
 for step in range(max_steps):
     t0 = time.time()
@@ -406,10 +480,27 @@ for step in range(max_steps):
             with open(log_file, "a") as f:
                 f.write(f"{step} val {val_loss_accum.item():.4f}\n")
 
+            if step > 0 and (step % 500 == 0 or last_step):
+                # write model checkpoints
+                train_loader_checkpoint = {'current_shard': train_loader.current_shard, 'current_position': train_loader.current_position}
+                checkpoint_path = os.path.join(log_dir, f"model_{step:05d}.pt")
+                checkpoint = {
+                    'model': raw_model.state_dict(),
+                    'optimizer': optimizer.state_dict(),
+                    'config': raw_model.config,
+                    'step': step,
+                    'val_loss': val_loss_accum.item(),
+                    'train_loader': train_loader_checkpoint,
+                }
+                torch.save(checkpoint, checkpoint_path)
+
     # eval hellaswag once in a while
-    if (step % 250 == 0 or last_step) and (not use_compile): # execute every 250th iter, or at the last step, ONLY if not using compile
+    if (step % 250 == 0 or last_step): # execute every 250th iter, or at the last step, ONLY if not using compile
         num_correct_norm = 0
         num_total = 0
+        # Use unwrapped (uncompiled) model for evaluation
+        model_for_eval = unwrap_model(model)
+        model_for_eval.eval()
         for i, example in enumerate(iterate_examples("val")):
             # only process examples where i % dpp_world_size == ddp_rank
             if i % ddp_world_size != ddp_rank:
@@ -439,44 +530,7 @@ for step in range(max_steps):
             with open(log_file, "a") as f:
                 f.write(f"{step} hellaswag {acc_norm:.4f}\n")
 
-    # once in a while generate from the model (except step 0, which is noise)
-    # if you disable torch.compile, this code works fine
-    if ((step > 0 and step % 250 == 0) or last_step) and (not use_compile):
-        model.eval()
-        num_return_sequences = 4
-        max_length = 32
-        tokens = enc.encode("Hello, I'm a language model,")
-        tokens = torch.tensor(tokens, dtype=torch.long)
-        tokens = tokens.unsqueeze(0).repeat(num_return_sequences, 1)
-        xgen = tokens.to(device)
-        sample_rng = torch.Generator(device=device)
-        sample_rng.manual_seed(42 + ddp_rank)
-        while xgen.size(1) < max_length:
-            # forward the model to get the logits
-            with torch.no_grad():
-                with torch.autocast(device_type=device, dtype=torch.bfloat16):
-                    logits, loss = model(xgen) # (B, T, vocab_size)
-                # take the logits at the last position
-                logits = logits[:, -1, :] # (B, vocab_size)
-                # get the probabilities
-                probs = F.softmax(logits, dim=-1)
-                # do top-k sampling of 50 (huggingface pipeline default)
-                # topk_probs here becomes (5, 50), topk_indices is (5, 50)
-                topk_probs, topk_indices = torch.topk(probs, 50, dim=-1)
-                # select a token from the top-k probabilities
-                # note: multinomial does not demand the input to sum to 1
-                ix = torch.multinomial(topk_probs, 1, generator=sample_rng) # (B, 1)
-                # gather the corresponding indices
-                xcol = torch.gather(topk_indices, -1, ix) # (B, 1)
-                # append to the sequence
-                xgen = torch.cat((xgen, xcol), dim=1)
-        # print the generated text
-        for i in range(num_return_sequences):
-            tokens = xgen[i, :max_length].tolist()
-            decoded = enc.decode(tokens)
-            print(f"rank {ddp_rank} sample {i}: {decoded}")
-
-    # do one step of the optimization
+    # forward and backward pass
     model.train()
     optimizer.zero_grad()
     loss_accum = 0.0
